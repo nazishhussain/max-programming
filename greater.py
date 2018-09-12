c49ca49b859f106f648482f3261a72f9c6356d85
nazishhussain <42990089+nazishhussain@users.noreply.github.com>
2018-09-12 10:30:57 +0530

    Add files via upload

---
+++ greater.py
@@ -0,0 +1,7 @@
+#FIND THE GREATEST OF TWO NUMBERS	
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+if(a>b):
+	print("A is greater than B")
+else:
+	print("B is greater than A")
