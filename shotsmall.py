2b36ddac30791cfd3c210a8a71a246ba9c980061
nazishhussain <42990089+nazishhussain@users.noreply.github.com>
2018-09-14 09:21:32 +0530

    Add files via upload

---
+++ shotsmall.py
@@ -0,0 +1,9 @@
+#write a program to compare any two numbers
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+if(a>b):
+	print("a is greater than b")
+else:	
+	print("b is greater than a")
+if(a==b):
+	print("a is equal to b")
