a80cc734f9f3227f8e2ff60d50d3136992de7d84
nazishhussain <42990089+nazishhussain@users.noreply.github.com>
2018-09-14 09:14:11 +0530

    Add files via upload

---
+++ small.py
@@ -0,0 +1,10 @@
+#write a program to compare any two numbers
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+if(a>b):
+	print("a is greater than b")
+else:	
+	if(a<b):
+		print("b is greater than a")
+	else:
+		print("a is equal to b")
