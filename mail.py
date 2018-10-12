9cabff80a83a3b30bb8866f5ababad3d4e68a2e1
nazishhussain <42990089+nazishhussain@users.noreply.github.com>
2018-10-12 10:17:30 +0530

    Add files via upload

---
+++ mail.py
@@ -0,0 +1,22 @@
+import smtplib
+import getpass
+myemail="nazish.hussain51@gmail.com"
+password=getpass.getpass()
+recemai = "vidya4821@gmial.com"
+# creats SMTP session
+s = smtplib.SMTP('smtp.gmail.com', 587)
+
+#start TLS for security
+s.starttls()
+
+#AUTHENTICATION
+s.login(myemail, password)
+
+#message to be sent
+message = "how are you"
+
+#sending the mail 
+s.sendmail("nazish.hussain51@gmail.com","vidya4821@gmail.com",message)
+
+#terminating the session
+s.quit()
