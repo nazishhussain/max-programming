cdc6a50e544b834d31ea3065beaa040acf81882a
nazishhussain <42990089+nazishhussain@users.noreply.github.com>
2018-10-31 10:34:08 +0530

    Add files via upload

---
+++ graph2.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+#x cord of l side of bars
+left=[1,2,3,4,5]
+
+#he
+#label of bars
+tick_label=['a','b','c','d','e']
+x=[1,2,3]
+y=[1,4,9]
+plt.plot(x,y)
+plt.xlabel("X Axis")
+plt.ylabel("Y Axis")
+plt.title("My first graphh")
+plt.show()
